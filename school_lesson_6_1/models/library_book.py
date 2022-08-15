e73f22048b89fa620127fe5e3e08f96d3fb6fea0
omkravets <omkravets@gmail.com>
2022-08-15 23:52:17 +0300

    [IMP] school_lesson_6_1: full commit

---
+++ school_lesson_6_1/models/library_book.py
@@ -0,0 +1,38 @@
+from odoo import api, fields, models
+
+
+class LibraryBook(models.Model):
+    _name = 'library.book'
+    _description = 'Library Books'
+
+    name = fields.Char('Title')
+    reader_id = fields.Many2one(
+        comodel_name='res.partner',
+        string='Reader',
+    )
+    taken_date = fields.Date('In use from', readonly=True)
+    user_id = fields.Many2one(
+        comodel_name='res.users',
+        string='Responsible',
+        default=lambda self: self.env.ref('base.user_demo').id,
+    )
+    active = fields.Boolean(
+        string='Active',
+        default=True,
+    )
+
+    category_id = fields.Many2one(
+        string="Book category",
+        comodel_name="library.book.category",
+    )
+
+    @api.onchange('reader_id')
+    def _onchange_reader_id(self):
+        if self.reader_id and not self.taken_date:
+            self.taken_date = fields.Date.today()
+
+    def action_assign_default(self):
+        self.ensure_one()
+        self.reader_id = self.env.ref(
+            'school_lesson_6_1.res_partner_customer').id
+
