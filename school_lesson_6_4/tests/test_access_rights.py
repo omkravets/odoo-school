5975c94a3f707957d7b01382e3c1dc808d1da6eb
omkravets <omkravets@gmail.com>
2022-08-19 23:37:13 +0300

    [IMP] school_lesson_6_4: add module 6_4

---
+++ school_lesson_6_4/tests/test_access_rights.py
@@ -0,0 +1,27 @@
+from odoo.addons.school_lesson_6_4.tests.common import TestCommon
+from odoo.tests import tagged
+from odoo.exceptions import AccessError
+
+
+@tagged('post_install', '-at_install', 'library', 'access')
+class TestAccessRights(TestCommon):
+
+    def test_01_library_user_access_rights(self):
+        with self.assertRaises(AccessError):
+            self.env['library.book'].with_user(self.library_user).create(
+                {'name': 'Test Book'})
+        with self.assertRaises(AccessError):
+            self.book_demo.with_user(self.library_user).unlink()
+        with self.assertRaises(AccessError):
+            self.env['library.author'].whith_user(self.library_user).create(
+                {'first_name': 'First',
+                 'last_name': 'Author'}
+            )
+
+
+    def test_02_library_admin_access_rights(self):
+        book = self.env['library.book'].with_user(self.library_admin).create(
+            {'name': 'Test Book'})
+        book.with_user(self.library_admin).read()
+        book.with_user(self.library_admin).write({'name': 'Test Book II'})
+        book.with_user(self.library_admin).unlink()
